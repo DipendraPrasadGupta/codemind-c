a1a837be8e250f06cc533e4883bd5e99b0c75909
DipendraPrasadGupta <119841165+DipendraPrasadGupta@users.noreply.github.com>
2022-12-25 01:29:11 +0545

    #include<stdio.h> #include<math.h> int main() {     int n,a;     scanf("%d ",&n);     while(n!=-1)
        {
            a=n*n;
            printf("%d
    ",a);
            scanf("%d",&n);
        }
    }

---
+++ Infinite.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,a;
+    scanf("%d
+",&n);
+    while(n!=-1)
+    
+    {
+        a=n*n;
+        printf("%d
+",a);
+        scanf("%d",&n);
+    }
+}
