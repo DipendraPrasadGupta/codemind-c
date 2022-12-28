102e9dcfd2900d6c8f06e7459c9385ef0bbefcda
Dipendra Prasad Gupta <119841165+DipendraPrasadGupta@users.noreply.github.com>
2022-12-28 11:50:36 +0545

    #include<stdio.h> int main() {     int n,c=0,d=0;     scanf("%d",&n);     int a[n];     for(int i=0; i<n; i++)     {         scanf("%d",&a[i]);     }     for(int j=0; j<n; j++)     {         if(j%2==0)         {             if(a[j]%2==0)             {                 c++;             }         }         else         {             if(a[j]%2==0)             {                 c=0;             }         }     }    if(c!=0)     {        printf("True");     }     else     {         printf("False");     } }

---
+++ Strictly_EVEN.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int n,c=0,d=0;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int j=0; j<n; j++)
+    {
+        if(j%2==0)
+        {
+            if(a[j]%2==0)
+            {
+                c++;
+            }
+        }
+        else
+        {
+            if(a[j]%2==0)
+            {
+                c=0;
+            }
+        }
+    }
+   if(c!=0)
+    {
+       printf("True"); 
+    }
+    else
+    {
+        printf("False");
+    }
+}
