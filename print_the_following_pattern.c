65e33151c35600ae743ca5b4388f9edeaef3c397
Dipendra Prasad Gupta <119841165+DipendraPrasadGupta@users.noreply.github.com>
2022-12-31 22:16:30 +0545

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1; i<=n; i++)     {         for(j=1; j<=i; j++)         {             if(j==1 || j==i || i==n)             {                printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
-
 int main()
 {
-	int i,j,n,k,l=1;
-	scanf("%d",&n);
-	for(i=1; i<=n; i++)
-	{
-		for(j=n-1; j>=i; j--)
-		{
-			printf(" ");
-		}
-		
-		for(k=1; k<=l; k++)
-		{ 
-			printf("%d",i);
-		}            
-			l = l+2;    
-	    printf("
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            if(j==1 || j==i || i==n)
+            {
+               printf("*");  
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("
 ");
-	}
+    }
 }
