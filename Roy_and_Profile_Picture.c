254dfaa6de1ae74852f7971509cd3d40f033a6a1
Dipendra Prasad Gupta <119841165+DipendraPrasadGupta@users.noreply.github.com>
2023-01-02 00:56:30 +0545

    #include<stdio.h> int main() {     int l,n,w,h;     scanf("%d%d",&l,&n);
        for(int i=1; i<=n; i++)
        {
            int w,h;
            scanf("%d%d",&w,&h);
            if(h<l || w<l)
            {
                printf("UPLOAD ANOTHER
    ");
            }
            else if(w==h)
            {
                printf("ACCEPTED
    ");
            }
            else
            {
                printf("CROP IT
    ");
            }
        }
    }

---
+++ Roy_and_Profile_Picture.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int l,n,w,h;
+    scanf("%d%d",&l,&n);
+   
+    for(int i=1; i<=n; i++)
+    {
+        int w,h;
+        scanf("%d%d",&w,&h);
+        if(h<l || w<l)
+        {
+            printf("UPLOAD ANOTHER
+");
+        }
+        else if(w==h)
+        {
+            printf("ACCEPTED
+");
+        }
+        else
+        {
+            printf("CROP IT
+");
+        }
+    }
+}
